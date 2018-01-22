cc58c96b990bb2364f53bd014461d0ce6fd49fb4
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2018-01-22 14:31:02 +0100

    searchesController and shortcutsController implemented

--- server/controllers/shortcutsController.js
+++ server/controllers/shortcutsController.js
@@ -1,25 +1,28 @@
 const monk = require('monk');
 const nconf = require('../configuration.js');
 const db = monk(nconf.get('MONGODB_URL') || 'localhost/polipro');
+// TODO: change in the real db
 
-const Sc = db.get('shortcuts');
+const User = db.get('users');
 
-
-module.exports.getShortcuts = async (ctx, next) =>{
-  if ('GET' != ctx.method) return await next();
-  const res = await Sc.find();
-  ctx.response.body =  res.sort(function (a, b){return  b.score - a.score});
+module.exports.postShortcuts = async (ctx) =>{
+  if ('POST' != ctx.method) return await next();
+  let shortcutsList = ctx.user.shortcutsList.push(ctx.request.body)
+  await User.update({email: ctx.user.email}, {'shortcutsList': shortcutsList});
+  ctx.response.body = shortcutsList;
   ctx.status=200;
 };
 
-module.exports.postShortcuts = async (ctx) =>{
-
-};
-
-module.exports.deleteShortcuts = async (ctx) =>{
-
-};
-
 module.exports.editShortcuts = async (ctx) =>{
+  if ('PUT' != ctx.method) return await next();
+  await User.update({email: ctx.user.email}, {'shortcutsList': ctx.request.body});
+  ctx.response.body = shortcutsList;
+  ctx.status=200;
+};
 
+module.exports.resetShortcuts = async (ctx) =>{
+  if ('PUT' != ctx.method) return await next();
+  await User.update({email: ctx.user.email}, {'shortcutsList': ctx.request.body});
+  ctx.response.body = shortcutsList;
+  ctx.status=200;
 };
