c4876a27763523649feade703f1e5dff66cd2185
Selyuu <rainierlouis@gmail.com>
2018-01-22 10:15:36 +0100

    feat: modaladd implemented

--- client/src/Components/ModalAdd.js
+++ client/src/Components/ModalAdd.js
@@ -4,36 +4,85 @@ import Modal, { closeStyle } from 'simple-react-modal';
 class ModalAdd extends Component {
  constructor(props) {
   super(props);
+  this.state = {
+   shortcut: '',
+   url_link: ''
+  };
  }
+
+ enterKey = e => {
+  this.setState({
+   [e.target.id]: e.target.value
+  });
+  console.log(this.state);
+  if (
+   e.which === 13 &&
+   this.state.url_link !== '' &&
+   this.state.shortcut !== ''
+  ) {
+   // send db
+  }
+ };
+
+ closeModal = f => {
+  this.setState({
+   shortcut: '',
+   url_link: ''
+  });
+  f();
+ };
 
  render() {
   return (
    <div className="modal_window">
     <Modal
      closeOnOuterClick={true}
-     onClose={this.props.close}
+     onClose={() => this.closeModal(this.props.close)}
      style={{}}
-     containerStyle={{ width: '40vw' }}
+     containerStyle={{ width: '40vw', padding: '30px 40px' }}
      show={this.props.show}
     >
-     <div className="modal_subwindow">
-      <h1 style={{ fontFamily: 'Avenir' }}>New Shortcut:</h1>
-      <h3
+     <div
+      onKeyUp={e => this.enterKey(e)}
+      style={{ display: 'flex', flexDirection: 'column' }}
+     >
+      <span
+       style={{
+        fontFamily: 'Avenir',
+        fontWeight: 800,
+        fontSize: '1.8em',
+        marginBottom: '10px'
+       }}
+      >
+       New Shortcut:
+      </span>
+      <span
        style={{
         color: '#aaa',
         fontFamily: 'Avenir',
         fontWeight: 300,
-        paddingTop: 0
+        marginBottom: '10px',
+        fontSize: '1.1em'
        }}
       >
        Insert the url of the website you'd like to add and assign it to a
        letter.
-      </h3>
+      </span>
       <div
        style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
       >
-       <h2 style={{ fontFamily: 'Avenir' }}>URL:</h2>
+       <h2
+        style={{
+         fontFamily: 'Avenir',
+         color: '#aaa',
+         fontWeight: '300',
+         marginRight: '10px'
+        }}
+       >
+        URL:
+       </h2>
        <input
+        id="url_link"
         style={{
          marginLeft: '10px',
          width: '600px',
@@ -43,25 +92,41 @@ class ModalAdd extends Component {
          boxShadow: '0 0 5px 2px rgba(50,50,50,0.1)',
          outlineOffset: 'none',
          outline: 'none',
-         outlineColor: 'none'
+         outlineColor: 'none',
+         fontSize: '1.2em',
+         paddingLeft: '15px'
         }}
        />
       </div>
       <div
        style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
       >
-       <h2 style={{ fontFamily: 'Avenir' }}>CTRL +</h2>
-       <input
+       <h2
         style={{
+         fontFamily: 'Avenir',
+         color: '#aaa',
+         fontWeight: '300',
+         marginRight: '10px'
+        }}
+       >
+        CTRL +
+       </h2>
+       <input
+        maxLength={1}
+        id="shortcut"
+        style={{
+         paddingLeft: '20px',
          marginLeft: '10px',
-         width: '100px',
+         width: '40px',
          height: '40px',
          outlineColor: 'none',
          border: 'none',
          boxShadow: '0 0 5px 2px rgba(50,50,50,0.1)',
          outlineOffset: 'none',
          outline: 'none',
-         outlineColor: 'none'
+         outlineColor: 'none',
+         fontSize: '1.2em',
+         textTransform: 'uppercase'
         }}
         className="modal_input_ctrl"
        />
