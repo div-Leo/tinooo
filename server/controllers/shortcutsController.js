2ec6f560292dd655c8e7ac3df554553185f09f20
Selyuu <rainierlouis@gmail.com>
2018-01-22 16:03:19 +0100

    fix: updated set method for db

--- server/controllers/shortcutsController.js
+++ server/controllers/shortcutsController.js
@@ -5,24 +5,33 @@ const db = monk(nconf.get('MONGODB_URL') || 'localhost/polipro');
 
 const User = db.get('users');
 
-module.exports.postShortcuts = async (ctx) =>{
-  if ('POST' != ctx.method) return await next();
-  let shortcutsList = ctx.user.shortcutsList.push(ctx.request.body)
-  await User.update({email: ctx.user.email}, {'shortcutsList': shortcutsList});
-  ctx.response.body = shortcutsList;
-  ctx.status=200;
+module.exports.postShortcuts = async ctx => {
+ if ('POST' != ctx.method) return await next();
+ ctx.user.shortcutsList.push(ctx.request.body.shortcut);
+ await User.update(
+  { email: ctx.user.email },
+  { $set: { shortcutsList: ctx.user.shortcutsList } }
+ );
+ ctx.response.body = ctx.user.shortcutsList;
+ ctx.status = 200;
 };
 
-module.exports.editShortcuts = async (ctx) =>{
-  if ('PUT' != ctx.method) return await next();
-  await User.update({email: ctx.user.email}, {'shortcutsList': ctx.request.body});
-  ctx.response.body = shortcutsList;
-  ctx.status=200;
+module.exports.editShortcuts = async ctx => {
+ if ('PUT' != ctx.method) return await next();
+ await User.update(
+  { email: ctx.user.email },
+  { $set: { shortcutsList: ctx.request.body } }
+ );
+ ctx.response.body = shortcutsList;
+ ctx.status = 200;
 };
 
-module.exports.resetShortcuts = async (ctx) =>{
-  if ('PUT' != ctx.method) return await next();
-  await User.update({email: ctx.user.email}, {'shortcutsList': ctx.request.body});
-  ctx.response.body = shortcutsList;
-  ctx.status=200;
+module.exports.resetShortcuts = async ctx => {
+ if ('PUT' != ctx.method) return await next();
+ await User.update(
+  { email: ctx.user.email },
+  { $set: { shortcutsList: ctx.request.body } }
+ );
+ ctx.response.body = shortcutsList;
+ ctx.status = 200;
 };
