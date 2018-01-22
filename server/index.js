326df7ad649f805a0f4fb30be809c61e852851f3
Selyuu <rainierlouis@gmail.com>
2018-01-22 16:03:29 +0100

    fix: updated user auth

--- server/index.js
+++ server/index.js
@@ -10,14 +10,19 @@ const nconf = require('./configuration.js');
 const db = monk(nconf.get('MONGODB_URL') || 'localhost/polipro');
 const User = db.get('users');
 
-app.use(cors())
-  .use(bodyparser())
-  .use(async (ctx, next) => {
-    let authorization = ctx.headers.authorization;
-    if (!authorization || authorization.split(' ')[0] != 'Bearer') return await next();
-    ctx.token = authorization.split(' ')[1];
-    ctx.user = await User.findOne({accessToken: ctx.token});
-    return await next();
-  })
-  .use(router.routes())
-  .listen(3001);
+app
+ .use(cors())
+ .use(bodyparser())
+ .use(async (ctx, next) => {
+  console.log('AUTH HEADER!=====', ctx.headers.authorization);
+  let authorization = ctx.headers.authorization;
+  if (!authorization || authorization.split(' ')[0] != 'Bearer')
+   return await next();
+  ctx.token = authorization.split(' ')[1];
+  console.log('CTX TOKEN=========', ctx.token);
+  ctx.user = await User.findOne({ accessToken: ctx.token });
+  console.log('CTX USER============', ctx.user);
+  return await next();
+ })
+ .use(router.routes())
+ .listen(3001);
