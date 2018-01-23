5eb7bcc1b6ecc4252775607fc0526d1847c5cb19
Selyuu <rainierlouis@gmail.com>
2018-01-23 16:58:33 +0100

    fix: changed user searches method

--- server/controllers/searchesController.js
+++ server/controllers/searchesController.js
@@ -7,13 +7,19 @@ const User = db.get('users');
 
 module.exports.postSearchesHistory = async ctx => {
  if ('POST' != ctx.method) return await next();
- let searchesList = ctx.user.searchesHistory.push(ctx.request.body);
- await User.update({ email: ctx.user.email },{ $set: { searchesHistory: searchesList } });
+ ctx.user.searchesList.push(ctx.request.body);
+ await User.update(
+  { email: ctx.user.email },
+  { $set: { searchesHistory: ctx.user.searchesList } }
+ );
  ctx.status = 200;
 };
 
 module.exports.resetSearchesHistory = async ctx => {
  if ('DELETE' != ctx.method) return await next();
- await User.update({ email: ctx.user.email },{ $set: { searchesHistory: ' ' } });
+ await User.update(
+  { email: ctx.user.email },
+  { $set: { searchesHistory: ' ' } }
+ );
  ctx.status = 200;
 };
